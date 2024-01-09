bbbd08b6ecd3c77cc5c4003b4992f1c323c566dd
Loki7003 <80339875+Loki7003@users.noreply.github.com>
2024-01-09 20:00:52 +0100

    app.js updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const fromAnotherFile = require("./js/func.js")
 const express = require("express");
 const exphbs = require('express-handlebars');
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'hbs')
 
