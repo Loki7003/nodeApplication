fb2a7243193b7bdfee5ba028ed043e27eb3d5c11
Loki7003 <80339875+Loki7003@users.noreply.github.com>
2024-01-09 18:57:27 +0100

    Heroku deployed

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-const fromAnotherFile = require("./functions")
+const fromAnotherFile = require("./js/func.js")
 const express = require("express");
+const exphbs = require('express-handlebars');
 const app = express();
 const port = process.env.port || 3000;
 
-app.set("view engine", 'hbs')
+app.set('view engine', 'hbs')
 
 app.get('/', function (req, res) {
  const sample = () => {
  return 'proba'
  }
  res.render('index', {
- pageTitle: "Lekcja01",
+ pageTitle: "Our Website",
  subTitle: fromAnotherFile.someTitle,
  anotherTitle: sample()
  })
 })
 
 app.get('/about', function (req, res) {
- res.send("My site")
+ res.send("Our Website")
 })
 
-var path = require('path')
+const path = require('path')
 app.use('/assets', express.static(path.join(__dirname, "./assets")));
 
-app.use('/js', express.static(path.joisssn(__dirname, "./js")));
+app.use('/js', express.static(path.join(__dirname, "./js")));
+
 
 app.listen(port, (err) => {
  if (err) {
